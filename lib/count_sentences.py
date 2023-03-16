c3f7729b6a1e2681f3e3940cebe33b06a2e6ec3a
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-03-16 14:28:25 -0400

    tests passing

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self.value = value
+
+  def get_value(self):
+    return self._value
+
+  def set_value(self, string):
+    if (type(string) in (str,)):
+      self._value = string
+    else:
+      print('The value must be a string.')
+
+  def is_sentence(self):
+    return self._value.endswith('.')
+  
+  def is_question(self):
+    return self._value.endswith('?')
+  
+  def is_exclamation(self):
+    return self._value.endswith('!')
+  
+  def count_sentences(self):
+    value = self._value
+    for punc in ['!', '?']:
+      value = value.replace(punc, '. ')
+      sentences = [sentence for sentence in value.split('.') if sentence]
+      
+      # print(len(sentences))
+      return(len(sentences))
+    
+
+
+  value = property(get_value, set_value)
+
+# test_string = MyString('Hello, my name is Finn. I am sad! Do I like food?')
